e2c5c5409943709f28f1d84b60a8031952e1727a
Doskarayev Nurlan <n.doskaraev@agrostream.net>
2019-06-24 00:04:45 +0500

    hjgj

--- AktobeInteractive/MainWindow.xaml.cs
+++ AktobeInteractive/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System.Globalization;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace AktobeInteractive
 {
@@ -10,42 +12,64 @@ namespace AktobeInteractive
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const int CellsCount = 100;
+        private const int CellsCount = 20;
+        private const int GridX0 = 25;
+        private const int GridY0 = 1;
+        public double WindowWidth => SystemParameters.PrimaryScreenWidth;
+        public double WindowHeight => SystemParameters.PrimaryScreenHeight;
+        public double CellWidth => MapGrid.Width / CellsCount;
+        public double CellHeight => MapGrid.Height / CellsCount;
         public MainWindow()
         {
             InitializeComponent();
+            MapGrid.Width = WindowWidth / 100 * 45;
+            MapGrid.Height = WindowHeight / 100 * 80;
             SetCoordinates();
         }
 
         private void SetCoordinates()
         {
-            var width = MapGrid.ActualWidth / CellsCount;
-            var height = MapGrid.ActualHeight / CellsCount;
             for (var i = 0; i < CellsCount; i++)
             {
-                var columnDefinition = new ColumnDefinition {Width = new GridLength(width, GridUnitType.Pixel)};
+                var columnDefinition = new ColumnDefinition {Width = new GridLength(CellWidth, GridUnitType.Pixel)};
                 MapGrid.ColumnDefinitions.Add(columnDefinition);
-                var rowDefinition = new RowDefinition {Height = new GridLength(height, GridUnitType.Pixel)};
+                var rowDefinition = new RowDefinition {Height = new GridLength(CellHeight, GridUnitType.Pixel)};
                 MapGrid.RowDefinitions.Add(rowDefinition);
             }
+            SetPoint(14, 8);
+        }
+
+        private void SetPoint(int x, int y)
+        {
+            var brush = new ImageBrush
+            {
+                ImageSource = new BitmapImage(
+                    new Uri("pack://application:,,,/AktobeInteractive;component/Resources/icon.png")),
+                Stretch = Stretch.Uniform
+            };
+            var point = new Border
+            {
+                Background = brush,
+                Height = CellHeight,
+                Width = CellWidth,
+            };
+            Grid.SetColumn(point, x);
+            Grid.SetRow(point, y);
+            MapGrid.Children.Add(point);
         }
 
         private void MapGrid_OnMouseDown(object sender, MouseButtonEventArgs e)
         {
+#if !DEBUG
             var pointToWindow = Mouse.GetPosition(this);
             var x = pointToWindow.X;
             var y = pointToWindow.Y;
-            var windowWidth = this.ActualWidth;
-            var windowHeight = this.ActualHeight;
-            var gridX0 = 55;
-            var gridY0 = 10;
-            var gridX = x - windowWidth / 100 * gridX0;
-            var gridY = y - windowHeight / 100 * gridY0;
-            var width = MapGrid.ActualWidth / CellsCount;
-            var height = MapGrid.ActualHeight / CellsCount;
-            var column = gridX / width;
-            var row = gridY / height;
+            var gridX = x - WindowWidth / 100 * GridX0;
+            var gridY = y - WindowHeight / 100 * GridY0;
+            var column = gridX / CellWidth;
+            var row = gridY / CellHeight;
             MessageBox.Show($"{(int) column}:{(int) row}");
+#endif
         }
     }
 }
