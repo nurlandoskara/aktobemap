1f5562498b5ada843894229479de980cb25fc3b4
Doskarayev Nurlan <n.doskaraev@agrostream.net>
2019-06-23 20:55:04 +0500

    jndsfjs

--- AktobeInteractive/MainWindow.xaml.cs
+++ AktobeInteractive/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System.Globalization;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace AktobeInteractive
 {
@@ -23,7 +25,24 @@ namespace AktobeInteractive
                 var columnDefinition = new ColumnDefinition {Width = new GridLength(CellsCount, GridUnitType.Pixel)};
                 MapGrid.ColumnDefinitions.Add(columnDefinition);
             }
+        }
 
+        private void MapGrid_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var pointToWindow = Mouse.GetPosition(this);
+            var x = pointToWindow.X;
+            var y = pointToWindow.Y;
+            var windowWidth = this.ActualWidth;
+            var windowHeight = this.ActualHeight;
+            var gridX0 = 55;
+            var gridY0 = 10;
+            var gridX = x - windowWidth / 100 * gridX0;
+            var gridY = y - windowHeight / 100 * gridY0;
+            var width = MapGrid.ActualWidth / CellsCount;
+            var height = MapGrid.ActualHeight / CellsCount;
+            var column = gridX / width;
+            var row = gridY / height;
+            MessageBox.Show($"{(int) column}:{(int) row}");
         }
     }
 }
