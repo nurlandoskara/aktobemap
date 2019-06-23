a42810dffe636fdd69031e43538d17def669f114
Doskarayev Nurlan <n.doskaraev@agrostream.net>
2019-06-23 20:12:11 +0500

    Add project files.

---
+++ AktobeInteractive/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AktobeInteractive
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int CellsCount = 100;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SetCoordinates()
+        {
+            var width = MapGrid.ActualWidth / CellsCount;
+            var height = MapGrid.ActualHeight / CellsCount;
+            for (var i = 0; i < CellsCount; i++)
+            {
+                var columnDefinition = new ColumnDefinition {Width = new GridLength(CellsCount, GridUnitType.Pixel)};
+                MapGrid.ColumnDefinitions.Add(columnDefinition);
+            }
+
+        }
+    }
+}
